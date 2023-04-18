0e555fa8ec06a8e6710b58fcb70ba2ffb4425291
GiulioDangelo <dangelogiulio48@gmail.com>
2023-04-18 15:40:09 +0200

    variable name edit

--- snack 2.2/script.js
+++ snack 2.2/script.js
@@ -1,14 +1,11 @@
 const firstName = ["giulio", "marco", "francesco", "pippo"];
-
 const lastName = ["fdds", "svzxcv", "dfds", "dsafwedxv"];
 
-let lastNameGenerator = Math.floor(Math.random() * (lastName.length - 1)) + 1;
-let nameGenerator = Math.floor(Math.random() * (firstName.length - 1)) + 1;
+let randomLastName = Math.floor(Math.random() * (lastName.length - 1)) + 1;
+let randomName = Math.floor(Math.random() * (firstName.length - 1)) + 1;
 
-const list = firstName[nameGenerator] + ' ' + lastName[lastNameGenerator];
-console.log(list)
+const generator = firstName[randomName] + ' ' + lastName[randomLastName];
 
-
-for (let i = 0; i <= 10; i++) {
-    console.log(list)
+for (let i = 0; i <= 15; i++) {
+    console.log(generator[i])
 }
