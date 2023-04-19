5b066ca35fe3835588eb8eacbffef1591e826641
GiulioDangelo <dangelogiulio48@gmail.com>
2023-04-19 11:30:02 +0200

    loop selector fix

--- snack 2.3/script.js
+++ snack 2.3/script.js
@@ -1,8 +1,8 @@
 const num = [1,2,3,4,5,6,7,8,9,10,]
 let odds = [];
 
-for (let i = 0; i <  num.length; i++) {
-    if (num[i] % 2 === 1) {
+for (let i = 0; i < num.length; i++) {
+    if (i % 2 === 1) {
         odds.push(num[i])
     }
 }
